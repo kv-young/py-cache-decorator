63c7545cab9a1015ecfd7a9e24a435825c04198b
kv-young <forever.move.kv@gmail.com>
2026-05-22 10:33:30 +0300

    implemented cache decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def inner(*args: tuple, **kwargs: dict) -> Any:
+        data = args, tuple(kwargs.items())
+        if data not in cache_dict:
+            print("Calculating new result")
+            cache_dict[data] = func(*args, **kwargs)
+        else:
+            print("Getting from cache")
+        return cache_dict[data]
+    return inner
